09fa38084d15d71d9008cc004898f1996ce02bb6
henryjxie <henryjxie@gmail.com>
2024-12-29 15:11:33 -0800

    Deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/threepointfive2627.github.io/', // Replace with your repo name
+  base: '/threepointfive/', // Replace with your repo name
   plugins: [react()],
 });
